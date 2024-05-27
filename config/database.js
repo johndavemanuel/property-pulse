65674f69ef56c940151e14b9389c3317cd1d283b
Dave Manuel <dave.manuel@stok.se>
2024-05-27 12:46:35 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -17,6 +17,7 @@ const connectDB = async () => {
     connected = true;
     console.log('MongoDB connected...');
   } catch (error) {
+    console.log('MongoDB error...');
     console.log(error);
   }
 };
