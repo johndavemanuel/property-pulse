66992f9a1eab297c0532b229b6b2891e8a320cfa
Dave Manuel <dave.manuel@stok.se>
2024-06-10 13:05:01 +0800

    chore: add comments to middleware

--- middleware.js
+++ middleware.js
@@ -1,5 +1,9 @@
+// The most simple usage is when you want to require authentication for your entire site.
+// You can add a middleware.js file with the following:
 export { default } from 'next-auth/middleware';
 
+// If you only want to secure certain pages, export a config object with a matcher:
+// protected routes if not logged in
 export const config = {
   matcher: ['/properties/add', '/profile', '/properties/saved', '/messages'],
 };
